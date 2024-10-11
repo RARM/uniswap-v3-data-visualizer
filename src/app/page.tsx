f8ed98bae8f313b281dda25657faa24cc403f8b5
Rodolfo Andres Rivas Matta <rodolforivasmatta@gmail.com>
2024-10-11 15:47:49 -0400

    Small update.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Header from "@/app/ui/header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <main className="flex flex-col items-center justify-center p-6">
+      </main>
     </>
   );
 }
