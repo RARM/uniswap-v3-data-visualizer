337b9686d02e95099a3afabb75a762e758af80bb
Rodolfo Andres Rivas Matta <rodolforivasmatta@gmail.com>
2024-10-11 15:06:45 -0400

    Add boilerplate documentation to the Visualizer class.

--- src/app/lib/visualizer.ts
+++ src/app/lib/visualizer.ts
@@ -1,7 +1,26 @@
+/**
+ * @file visualizer.ts
+ * @description This file contains the Visualizer class which controls the logic
+ * to retrieve data, transform it, and pass it to the UI Controller for
+ * rendering.
+**/
+
+/**
+ * Class representing a Visualizer.
+ * @class
+**/
 export default class Visualizer {
+  /**
+   * Create a Visualizer.
+   * @constructor
+  **/
   constructor() {}
 
-  getVisualizations() {
+  /**
+   * Get the list of visualizations.
+   * @returns {Array<Object>} An array of visualization objects.
+   */
+  getVisualizations(): Array<Object> {
     return [
       {
         name: "Liquidity",
