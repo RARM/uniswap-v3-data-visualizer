122ce37a8e7db0df217e38a9e139eb73e8dde044
Rodolfo Andres Rivas Matta <rodolforivasmatta@gmail.com>
2024-10-11 19:11:09 -0400

    Adding the visualizer to the main driver and testing ENV variables.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 // import Image from "next/image";
 import Header from "@/app/ui/header";
 import UIController from "./ui/ui-controller";
+import Visualizer from "./lib/visualizer";
 
 export default function Home() {
+  let dataController = new Visualizer();
+  
   return (
     <>
       <Header />
       <main className="flex flex-col items-center justify-center p-6">
         <UIController />
+        <p>The key variable is: { process.env.SUBGRAPH_KEY }</p>
       </main>
     </>
   );
